58c543b69d80491c24103a29bb5bbfc2548ce834
Daksh-coder-oss <vv0039190@gmail.com>
2024-11-16 11:09:08 +0000

    Add files via upload

---
+++ Whole_number.py
@@ -0,0 +1,6 @@
+num=int(input("Please enter a number"))
+sum=0
+
+for i in range(num+1):
+    sum=sum+i
+print("sum:",sum)    
---
+++ reverse.py
@@ -0,0 +1,8 @@
+string="Hello"
+string1=""
+for i in string:
+    print(i)
+    string1=i+string1
+print(string1)    
+    
+
